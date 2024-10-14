7216741cf45caca32b298a9809a2171dbe3695ec
YetAnotherZephyruso <176294927+YetAnotherZephyruso@users.noreply.github.com>
2024-10-14 10:47:09 +0800

    dist

---
+++ assets/global-BAW08WkX.js
@@ -0,0 +1,1 @@
+import{W as a,a$ as m}from"./index-CU8c4d7y.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
--- src/signals/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './config'
-export * from './connections'
-export * from './global'
-export * from './proxies'
-export * from './request'
-export * from './rules'
-export * from './theme'
